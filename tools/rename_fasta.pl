70b8284072f4d5310fec733ff8a069eeb62f4f0c
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-09-08 14:12:49 +0100

    rename_fasta.pl is a short perl script to rename fasta accessions in fasta files by appending the array of strings to the header.

---
+++ tools/rename_fasta.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# rename_fasta.pl
+# A module that appends given strings to the beginning of fasta accessions.
+
+my $usage = "rename_fasta.pl <infile.fa> <text_to_append> <more_text_to_append>";
+my $infile = shift or die $usage;
+my $addstring = join ('_', @ARGV) or die $usage;
+
+open INFILE, $infile;
+
+while (my $line = <INFILE>) {
+	if ($line =~ s/>//) { print ">$addstring"."_$line";	}
+	else { print "$line"; }
+}
+
+exit;
+
+
+
+

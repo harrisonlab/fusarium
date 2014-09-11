f834a96d1bd4a927619802850d58fc85be564e5a
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-09-11 11:39:38 +0100

    A) Now runs tiling2gff.pl, a perl script to parse MUMmer output .tiling files to .gff files. B) Now requires additional arguement to set whether the program uses NUCmer or PROmer.

--- pathogen/identify_LS_chromosomes/identify_ls_contigs_MUMmer.sh
+++ pathogen/identify_LS_chromosomes/identify_ls_contigs_MUMmer.sh
@@ -4,19 +4,20 @@
 #$ -pe smp 1
 #$ -l virtual_free=0.9G
 
-USAGE="identify_ls_contigs_MUMmer.sh <reference_contigs.fa> <assembled_contigs.fa>"
+USAGE="identify_ls_contigs_MUMmer.sh <[nucmer/promer]> <reference_contigs.fa> <assembled_contigs.fa>"
 
 CUR_PATH=$PWD
 
-REF_CONTIGS=$1
-QRY_CONTIGS=$2
+MER_TYPE=$1
+REF_CONTIGS=$2
+QRY_CONTIGS=$3
 
 IN_REF=$(echo $REF_CONTIGS | rev | cut -d "/" -f1 | rev)
 
 ORGANISM=$(echo $QRY_CONTIGS | rev | cut -d "/" -f4 | rev)
 STRAIN=$(echo $QRY_CONTIGS | rev | cut -d "/" -f3 | rev)
 IN_QRY=$(echo $QRY_CONTIGS | rev | cut -d "/" -f1 | rev)
-OUTFILE=$(echo $QRY_CONTIGS | sed s/.aa//)
+OUTFILE="$STRAIN"_"$MER_TYPE"
 
 WORK_DIR=$TMPDIR/"$STRAIN"_MUMmer
 mkdir -p $WORK_DIR
@@ -25,9 +26,9 @@ cd $WORK_DIR
 cp $CUR_PATH/$REF_CONTIGS .
 cp $CUR_PATH/$QRY_CONTIGS .
 
-nucmer --prefix="$STRAIN"_MUMmer $IN_REF $IN_QRY 
-show-coords -rcl "$STRAIN"_MUMmer.delta > "$STRAIN"_MUMmer.coords
-show-tiling "$STRAIN"_MUMmer.delta > "$STRAIN"_MUMmer.tiling
+$MER_TYPE --prefix=$OUTFILE $IN_REF $IN_QRY 
+show-coords -rcl $OUTFILE.delta > $OUTFILE.coords
+show-tiling $OUTFILE.delta > $OUTFILE.tiling
 
 mkdir MUMmer_alignments
 
@@ -38,13 +39,18 @@ while read line; do
 	else
 		QRY_SEQ=$(echo $line | cut -d ' ' -f 8)
 		echo "qry_seq set to: $QRY_SEQ"
-		show-aligns "$STRAIN"_MUMmer.delta $REF_SEQ $QRY_SEQ > MUMmer_alignments/"$REF_SEQ"_"$QRY_SEQ".aligns
+		show-aligns $OUTFILE.delta $REF_SEQ $QRY_SEQ > MUMmer_alignments/"$REF_SEQ"_"$QRY_SEQ".aligns
 	fi
-done<"$STRAIN"_MUMmer.tiling
+done<$OUTFILE.tiling
+
+/home/armita/git_repos/emr_repos/scripts/fusarium/pathogen/identify_LS_chromosomes/tiling2gff.pl "$STRAIN"_"$MER_TYPE" $OUTFILE.tiling > $OUTFILE.gff
+
 
 mkdir -p $CUR_PATH/analysis/identify_LS_contigs/$ORGANISM/$STRAIN/
 rm $IN_REF $IN_QRY
 cp -r $WORK_DIR/ $CUR_PATH/analysis/identify_LS_contigs/$ORGANISM/$STRAIN/.
 rm -r $TMPDIR
+
+
 
 exit

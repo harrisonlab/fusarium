426c677e0d28edcf2ea5d41e1c41db0a2e011484
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-09-26 11:48:50 +0100

    Commands used to repeatmask fusarium genomes on 26.09.14

---
+++ repeat_masking/rep_mask_FoC.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/125/125_assembly.71/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/55/55_assembly.61/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/A23/A23_assembly.61/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/A28/A28_assembly.61/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/D2/D2_assembly.41/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_assembly.81/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/HB17/HB17_assembly.51/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/PG/PG_assembly.81/sorted_contigs.fa 
+
+
+

519b0cf444a6d82f7daf1e21749044b495b43894
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-11-05 10:13:17 +0000

    Commands as used for repeat masking in FoC

--- repeat_masking/rep_mask_FoC.sh
+++ repeat_masking/rep_mask_FoC.sh
@@ -13,7 +13,19 @@ qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modelin
 # then ran
 
 grep -v '#' repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked.fa.out.gff > repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked.gff
-/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked.gff -o repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked_fix.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/125/125_assembly.71_repmask/125_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/55/55_assembly.61_repmask/55_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/55/55_assembly.61_repmask/55_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/A23/A23_assembly.61_repmask/A23_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/A23/A23_assembly.61_repmask/A23_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/A28/A28_assembly.61_repmask/A28_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/A28/A28_assembly.61_repmask/A28_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/D2/D2_assembly.41_repmask/D2_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/D2/D2_assembly.41_repmask/D2_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_assembly.81_repmask/Fus2_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_assembly.81_repmask/Fus2_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/HB17/HB17_assembly.51_repmask/HB17_contigs_hardmasked.fa.out.gff -o -i repeat_masked/F.oxysporum_fsp_cepae/HB17/HB17_assembly.51_repmask/HB17_contigs_hardmasked.gff
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/PG/PG_assembly.81_repmask/PG_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/PG/PG_assembly.81_repmask/PG_contigs_hardmasked.gff
+
+
+
+
+
 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/F.oxysporum_fsp_cepae/125/125_assembly.71/sorted_contigs.fa 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/F.oxysporum_fsp_cepae/55/55_assembly.61/sorted_contigs.fa 

02eb68abe344a55375f5b960e3165be8e7ac5166
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-01-12 13:39:05 +0000

    old commits

--- assembly/Fus2_assembly.sh
+++ assembly/Fus2_assembly.sh
@@ -12,16 +12,17 @@ FORWARD_FILE1=$1
 REVERSE_FILE1=$2
 FORWARD_FILE2=$3
 REVERSE_FILE2=$4
-WORK_DIR=/tmp/Fus2_combined
+WORK_DIR=$TMPDIR/Fus2_combined
 
 mkdir -p $WORK_DIR
-velveth $WORK_DIR 41,61,2 -fastq -shortPaired -separate $FORWARD_FILE1 $REVERSE_FILE1 -shortPaired2 -separate $FORWARD_FILE2 $REVERSE_FILE2
-#velveth $WORK_DIR 61,101,2 -fastq -shortPaired -separate $FORWARD_FILE1 $REVERSE_FILE1 -shortPaired2 -separate $FORWARD_FILE2 $REVERSE_FILE2
+cd $WORK_DIR
+velveth $WORK_DIR 41,101,2 -fastq -shortPaired -separate $CUR_DIR/$FORWARD_FILE1 $CUR_DIR/$REVERSE_FILE1 -shortPaired2 -separate $CUR_DIR/$FORWARD_FILE2 $CUR_DIR/$REVERSE_FILE2
 for hash_directory in $(ls -d $WORK_DIR*); do
 	cd $hash_directory
 	velvetg . -exp_cov 133 -cov_cutoff 26 -ins_length1 300 -ins_length2 600 -min_contig_lgth 500
 	cd ../
 done
+
 cp -r $WORK_DIR* $CUR_DIR/assembly/velvet/F.oxysporum_fsp_cepae/Fus2/.
 
 rm -r $TMPDIR
--- assembly/Fus2_assembly_commands.sh
+++ assembly/Fus2_assembly_commands.sh
@@ -1,5 +1,19 @@
 qsub /home/armita/git_repos/emr_repos/scripts/fusarium/assembly/Fus2_assembly.sh qc_dna/paired/F.oxysporum_fsp_cepae/Fus2/Fus2_70bp_F.fastq qc_dna/paired/F.oxysporum_fsp_cepae/Fus2/Fus2_70bp_R.fastq qc_dna/paired/F.oxysporum_fsp_cepae/Fus2/Fus2_250_F.fastq qc_dna/paired/F.oxysporum_fsp_cepae/Fus2/Fus2_250_R.fastq
 
-for folder in $(ls -d Fus2_combined_*); do /home/armita/git_repos/emr_repos/tools/seq_tools/assemblers/assembly_stats.pl $folder/process_contigs/sorted_contigs.fa | tail -n 3 | cut -d ':' -f2; done
+
+for directory in $(ls -d assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_*); do process_contigs.pl -i $directory/contigs.fa -o $directory/process_contigs; done
+
+for directory in $(ls -d assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_*); do cat $directory/process_contigs/stats.txt | rev | cut -d ' ' -f1 | rev; done
+
+for directory in $(ls -d assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_*); do /home/armita/git_repos/emr_repos/tools/seq_tools/assemblers/assembly_stats.pl $directory/process_contigs/sorted_contigs.fa | tail -n 3 | cut -d ':' -f2; done
 
 grep 'Final graph' Fus2_assembly.sh.o6236768 | cut -d ' ' -f 15 | cut -d '/' -f1 
+
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/process_contigs/sorted_contigs.fa
+
+/home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/fix_rm_gff3.py -i repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/Fus2_contigs_hardmasked.fa.out.gff -o repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/Fus2_contigs_hardmasked.gff
+
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/process_contigs/sorted_contigs.fa
+
+SCRIPT_DIR=/home/armita/git_repos/emr_repos/tools/gene_prediction/augustus
+qsub $SCRIPT_DIR/submit_augustus.sh fusarium_graminearum repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/Fus2_combined_49_contigs_hardmasked.fa

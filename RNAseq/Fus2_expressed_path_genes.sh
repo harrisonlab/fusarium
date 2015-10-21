4fd89e26d3d341a9ab25a8bbcd07ff3fd38f189c
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-10-21 09:51:27 +0100

    Old edits

--- RNAseq/Fus2_expressed_path_genes.sh
+++ RNAseq/Fus2_expressed_path_genes.sh
@@ -34,7 +34,7 @@ cat $INTERPRO_DIR/Fus2_interproscan.gff3 | grep 'g13840.' | less
 # in peptide transport.
 # g13840.t1       Pfam    protein_match   181     476     7.6E-27 +       .       Name=PF00854;signature_desc=POT family;Target=g13840.t1 181 476;status=T;ID=match$196_181_476;Ontology_term="GO:0005215","GO:0006810","GO:0016020";date=06-01-2015;Dbxref="InterPro:IPR000109","Reactome:REACT_15518","Reactome:REACT_19419"
 # g13840.t1       SUPERFAMILY     protein_match   144     327     5.75E-13        +       .       Name=SSF103473;Target=g13840.t1 144 327;status=T;ID=match$201_144_327;date=06-01-2015;Dbxref="InterPro:IPR020846"
-# 1. Visual inspection of g12840 shows all the alignments 
+# 1. Visual inspection of g12840 shows all the alignments
 # between contig positions 9331 and 9381
 # This is probably part of an onion read.
 
@@ -43,7 +43,7 @@ cat $INTERPRO_DIR/Fus2_interproscan.gff3 | grep 'g13840.' | less
 # between aligned reads and the FUS2 genome. Certainly many
 # reads present covering the gene and the neighbouring region.
 
-# 3. g2042 is a ubiquitin family protein. Which is foundubiquitously in Eukaryotes. 
+# 3. g2042 is a ubiquitin family protein. Which is foundubiquitously in Eukaryotes.
 # - homology to onion?
 # Visual inspection of the reads indicate that there is high
 # heterogeneity between the aligned reads and the Fus2 genome.
@@ -53,8 +53,8 @@ cat $INTERPRO_DIR/Fus2_interproscan.gff3 | grep 'g13840.' | less
 # There is are some notable differences between the aligned reads and Fus2 genome.
 # There is apporximately 10 genomic 'errors' in the fist 983bp of the contig.
 
-for TIMEPOINT in 0 4 8 16 24.1 24.2 36 48 72 96; do 
-	mkdir -p expreiment1/F.oxysporum_fsp_cepae/Fus2/$TIMEPOINT 
+for TIMEPOINT in 0 4 8 16 24.1 24.2 36 48 72 96; do
+	mkdir -p expreiment1/F.oxysporum_fsp_cepae/Fus2/$TIMEPOINT
 done
 
 cp timecourse/expreiment1/1_R1_fus_q30_l50.fastq timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/0/.
@@ -91,4 +91,60 @@ for IN_SAM in $(ls timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/*/*.sam); d
 done
 
 
+for FILE in $(ls timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/*/*expressed_genes.bed); do
+	printf "$FILE\n" >> tmp3/top_50_genes.csv
+	cat $FILE | head -n50 >> tmp3/top_50_genes.csv
+done
 
+# identify is any of the top 100 expressed genes in Fus2 come from the same contig.
+cat timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/96/Fus2_96_expressed_genes.bed | head -n 100 | cut -f1 | sort | uniq -c | sort -r | less
+# 	  4 NODE_9643_length_1511754_cov_84.114418
+#       4 NODE_4583_length_445765_cov_84.597122
+#       4 NODE_1105_length_605632_cov_83.266510
+#       3 NODE_566_length_732593_cov_83.236145
+#       3 NODE_2374_length_16459_cov_83.673859
+#       3 NODE_1097_length_595114_cov_83.826759
+#       2 NODE_956_length_295543_cov_83.945419
+#       2 NODE_731_length_218737_cov_86.751648
+#       2 NODE_7096_length_311910_cov_84.936836
+#       2 NODE_6283_length_399598_cov_84.774551
+#       2 NODE_608_length_519399_cov_83.445702
+#       2 NODE_4974_length_475916_cov_84.779022
+#       2 NODE_4873_length_67562_cov_92.001808
+#       2 NODE_4575_length_487109_cov_84.320221
+#       2 NODE_256_length_160675_cov_85.558762
+#       2 NODE_2122_length_266153_cov_81.544640
+#       2 NODE_1420_length_454186_cov_84.973450
+#       2 NODE_10255_length_363655_cov_83.498451
+#       1 NODE_979_length_171603_cov_83.119377
+
+cat analysis/blast_homology/F.oxysporum_fsp_cepae/Fus2/Fus2_six-appended_parsed.fa_homologs.csv | cut -f 1,9,14,15 | sort -k2 | grep 'NODE' | cat
+# Fusarium_oxysporum_f._sp._lycopersici_SIX3_gene_for_Secreted_in_xylem_3_protein	NODE_1364_length_4671_cov_108.113892	1100	1657
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_14844_secreted_in_xylem_3_(SIX3)_gene,_complete_cds	NODE_1364_length_4671_cov_108.113892	1136	1624
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_BFOL-51_secreted_in_xylem_3_(SIX3)_gene,_complete_cds	NODE_1364_length_4671_cov_108.113892	1136	1624
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_FOL-MM10_secreted_in_xylem_3_(SIX3)_gene,_complete_cds	NODE_1364_length_4671_cov_108.113892	1136	1624
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_IPO3_secreted_in_xylem_3_(SIX3)_gene,_complete_cds	NODE_1364_length_4671_cov_108.113892	1136	1624
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_BFOL-51_secreted_in_xylem_5_(SIX5)_gene,_partial_cds	NODE_2080_length_2535_cov_50.341618	310	836
+# Fusarium_oxysporum_f._sp._lycopersici_Six5_mRNA,_complete_cds	NODE_2080_length_2535_cov_50.341618	706	843
+# Fusarium_oxysporum_f._sp._lilii_isolate_NRRL_28395_secreted_in_xylem_7-like_protein_(SIX7)_gene,_complete_cds	NODE_2938_length_1682_cov_52.927467	601	1266
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_BFOL-51_secreted_in_xylem_7_(SIX7)_gene,_complete_cds	NODE_2938_length_1682_cov_52.927467	601	1266
+# Fusarium_oxysporum_f._sp._lycopersici_secreted_in_xylem_Six7_(SIX7)_mRNA,_complete_cds	NODE_2938_length_1682_cov_52.927467	601	1266
+# Fusarium_oxysporum_f._sp._lycopersici_strain_Fol007_Six10_(SIX10)_mRNA,_complete_cds	NODE_788_length_4929_cov_54.397850	4328	4697
+# Fusarium_oxysporum_f._sp._lycopersici_strain_Fol007_Six12_(SIX12)_mRNA,_complete_cds	NODE_788_length_4929_cov_54.397850	750	1091
+#
+#In Summary:
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_14844_secreted_in_xylem_3_(SIX3)_gene,_complete_cds
+# NODE_1364_length_4671_cov_108.113892	1136	1624
+# Fusarium_oxysporum_f._sp._lycopersici_isolate_BFOL-51_secreted_in_xylem_5_(SIX5)_gene,_partial_cds
+# NODE_2080_length_2535_cov_50.341618	310	836
+# Fusarium_oxysporum_f._sp._lilii_isolate_NRRL_28395_secreted_in_xylem_7-like_protein_(SIX7)_gene,_complete_cds
+# NODE_2938_length_1682_cov_52.927467	601	1266
+# Fusarium_oxysporum_f._sp._lycopersici_strain_Fol007_Six10_(SIX10)_mRNA,_complete_cds
+# NODE_788_length_4929_cov_54.397850	4328	4697
+# Fusarium_oxysporum_f._sp._lycopersici_strain_Fol007_Six12_(SIX12)_mRNA,_complete_cds	NODE_788_length_4929_cov_54.397850	750	1091
+
+#Identify expression of Fusarium SIX genes.
+qsub /home/armita/git_repos/emr_repos/tools/pathogen/blast/blast_pipe.sh analysis/blast_homology/six_genes/six-appended_parsed.fa dna repeat_masked/F.oxysporum_fsp_cepae/Fus2/Fus2_combined_49/Fus2_combined_49_contigs_unmasked.fa
+/home/armita/git_repos/emr_repos/tools/pathogen/blast/blast2gff.pl six_gene analysis/blast_homology/F.oxysporum_fsp_cepae/Fus2/Fus2_six-appended_parsed.fa_homologs.csv > analysis/blast_homology/F.oxysporum_fsp_cepae/Fus2/Fus2_six-appended_parsed.fa_homologs.gff
+cat timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/96/Fus2_96_expressed_genes.bed | grep 'NODE_4546' | less
+bedtools intersect -c -a analysis/blast_homology/F.oxysporum_fsp_cepae/Fus2/Fus2_six-appended_parsed.fa_homologs.gff -b timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/96/Fus2_96_sorted.bam > timecourse/expreiment1/F.oxysporum_fsp_cepae/Fus2/96/Fus2_96_expressed_six.bed

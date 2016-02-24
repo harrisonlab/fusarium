2dbe57f87391384af5f31e57cb8e578a2f86f46d
adarmitage <Andrew.Armitage@EMR.ac.uk>
2016-02-24 17:59:02 +0000

    Now coloured. No labels

--- pathogen/orthology/FoC_path_vs_non_path_venn_diag.r
+++ pathogen/orthology/FoC_path_vs_non_path_venn_diag.r
@@ -7,13 +7,13 @@
  # building of the matrix using:
  # ~/git_repos/emr_repos/tools/pathogen/orthology/orthoMCL/orthoMCLgroups2tab.py
 
-# This script requires the optparse R package. This can be downloaded by opening
-# R and running the following command:
-# install.packages("optparse",repos="http://cran.uk.r-project.org")
-# When given the option, install this package to a local library.
+ # The script also requires the colorspace package. This can be downloaded by
+ # opening R and running the following command:
+ # install.packages(colorspace)
 
-#get config options
+ #get config options
 library(optparse)
+library(colorspace)
 library(VennDiagram, lib.loc="/home/armita/R-packages/")
 opt_list = list(
     make_option("--inp", type="character", help="tab seperated file containing matrix of presence of orthogroups"),
@@ -33,19 +33,19 @@ nonpath=subset(df1, df1[,"A28"] == 1 & df1[,"D2"] == 1 & df1[,"PG"] == 1 & df1[,
 path=subset(df1, df1[,"A28"] == 0 & df1[,"D2"] == 0 & df1[,"PG"] == 0 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1)
 orthologs=subset(df1, df1[,"A28"] == 1 & df1[,"D2"] == 1 & df1[,"PG"] == 1 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1)
 
-area1=(nrow(path) + nrow(orthologs))
-area2=(nrow(nonpath) + nrow(orthologs))
-area1
+area1=(nrow(nonpath) + nrow(orthologs))
+area2=(nrow(path) + nrow(orthologs))
 area2
-# print(area1, area2, area3, area4)
+area1
 
-# colname1 <- paste(colnames(df1)[1])
-# colname2 <- paste(colnames(df1)[2])
 
-# label1 <- paste(colname1, ' (', area1, ')', sep="" )
-# label2 <- paste(colname2, ' (', area2, ')', sep="" )
-label1 <- paste('Path', ' (', area1, ')', sep="" )
-label2 <- paste('NonPath', ' (', area2, ')', sep="" )
+# Print labels
+# label1 <- paste('Path', ' (', area2, ')', sep="" )
+# label2 <- paste('NonPath', ' (', area1, ')', sep="" )
+
+# No labels
+label1 <- paste("", sep="" )
+label2 <- paste("", sep="" )
 
 
 n12=nrow(subset(df1, df1[,"A28"] == 1 & df1[,"D2"] == 1 & df1[,"PG"] == 1 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1))
@@ -62,7 +62,7 @@ draw.pairwise.venn(area1, area2,
     lwd = rep(2, 2),
     lty = rep("solid", 2),
     col = rep("black", 2),
-    fill = NULL,
+    fill = c(rainbow_hcl(2)),
     alpha = rep(0.5, 2),
     label.col = rep("black", 3),
     cex = rep(1, 3),
@@ -93,43 +93,43 @@ dev.off()
 singles = df1[grepl("single*", rownames(df1)), ]
 print("A28")
 total_1 = nrow(subset (df1, df1[,"A28"] == 1))
-missing_1 = (total_1 - area1)
-uniq_1=sum(singles[, 1])
+missing_1 = (total_1 - area2)
+uniq_1=sum(singles[, "A28"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_1)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_1)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_1)
 print("D2")
 total_2 = nrow(subset (df1, df1[,"D2"] == 1))
-missing_2 = (total_2 - area1)
-uniq_2=sum(singles[, 2])
+missing_2 = (total_2 - area2)
+uniq_2=sum(singles[, "D2"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_2)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_2)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_2)
 print("PG")
 total_3 = nrow(subset (df1, df1[,"PG"] == 1))
-missing_3 = (total_3 - area1)
-uniq_3=sum(singles[, 3])
+missing_3 = (total_3 - area2)
+uniq_3=sum(singles[, "PG"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_3)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_3)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_3)
 print("Fus2")
 total_4 = nrow(subset (df1, df1[,"Fus2"] == 1))
-missing_4 = (total_4 - area2)
-uniq_4=sum(singles[, 4])
+missing_4 = (total_4 - area1)
+uniq_4=sum(singles[, "Fus2"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_4)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_4)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_4)
 print("125")
 total_5 = nrow(subset (df1, df1[,"125"] == 1))
-missing_5 = (total_5 - area2)
-uniq_5=sum(singles[, 5])
+missing_5 = (total_5 - area1)
+uniq_5=sum(singles[, "125"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_5)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_5)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_5)
 print("A23")
 total_6 = nrow(subset (df1, df1[,"A23"] == 1))
-missing_6 = (total_6 - area2)
-uniq_6=sum(singles[, 6])
+missing_6 = (total_6 - area1)
+uniq_6=sum(singles[, "A23"])
 paste('The total number of orthogroups and singleton genes in this isolate: ', total_6)
 paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_6)
 paste('The total number of singleton genes not in the venn diagram: ', uniq_6)

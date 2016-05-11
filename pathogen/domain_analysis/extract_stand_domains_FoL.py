09f487678a9037966bed2df6912019bf3d5b6147
adarmitage <Andrew.Armitage@EMR.ac.uk>
2016-05-11 09:42:36 +0100

    Now calculates gene density

--- pathogen/domain_analysis/extract_stand_domains_FoL.py
+++ pathogen/domain_analysis/extract_stand_domains_FoL.py
@@ -19,10 +19,12 @@ import argparse
 import re
 import numpy
 from sets import Set
+from Bio import SeqIO
 from collections import defaultdict
 from operator import itemgetter
 
 ap = argparse.ArgumentParser()
+ap.add_argument('--FoL_genome',required=True,type=str,help='The genome assembly of FoL 4287')
 ap.add_argument('--summary_table',required=True,type=str,help='The summary table from Fo)build_annot_table.py')
 ap.add_argument('--N_term_out',required=True,type=str,help='The output file detailing N terminal domains by chromosome')
 ap.add_argument('--NBD_out',required=True,type=str,help='The output file detailing NBD domains by chromosome')
@@ -34,8 +36,16 @@ file_N_term_out = open(conf.N_term_out, 'w')
 file_NBD_out = open(conf.NBD_out, 'w')
 file_C_term_out = open(conf.C_term_out, 'w')
 
+
 with open(conf.summary_table) as f:
     summary_table_lines = f.readlines()
+
+#-----------------------------------------------------
+# Step 1.5
+# Build a dictionary detailing all the annotations
+# that will be pulled out from interproscan annotations
+#-----------------------------------------------------
+
 
 pfam_domain_dict = {
 # N-terminal domains:
@@ -94,6 +104,8 @@ pfam_domain_dict = {
 # Step 2
 # Extract interproscan information on NLR candidate
 # proteins
+# This section also records the number of genes on
+# chromsome / region
 #-----------------------------------------------------
 
 NLR_prot_dict = defaultdict(list)
@@ -160,6 +172,68 @@ for line in summary_table_lines:
             i += 1
             contig_dict[Fol_contig].append((combination + "\t" + store_line))
 
+
+#-----------------------------------------------------
+# Step 2
+# Identify the length and gene density of FoL chromosomes
+#-----------------------------------------------------
+
+contig_length_dict = defaultdict(list)
+
+genome_file = open(conf.FoL_genome, 'r')
+for cur_record in SeqIO.parse(genome_file,"fasta"):
+    seq_id = cur_record.id
+    if seq_id == '1':
+        chr1_core = cur_record.seq[0:FoL_Chr1_LS_start]
+        chr1_LS = cur_record.seq[FoL_Chr1_LS_start:]
+        seq_len_core = len(chr1_core)
+        seq_len_LS = len(chr1_LS)
+        N_count_core = chr1_core.count('N')
+        N_count_LS = chr1_LS.count('N')
+        seq_len_no_Ns_core = int(seq_len_core) - int(N_count_core)
+        seq_len_no_Ns_LS = int(seq_len_LS) - int(N_count_LS)
+        gene_density_core = numpy.divide(float(gene_count_dict["1_core"]), float(seq_len_core)) * 1000000
+        gene_density_LS = numpy.divide(float(gene_count_dict["1_LS"]), float(seq_len_LS)) * 1000000
+        gene_density_no_Ns_core = numpy.divide(float(gene_count_dict["1_core"]), float(seq_len_no_Ns_core)) * 1000000
+        gene_density_no_Ns_LS = numpy.divide(float(gene_count_dict["1_LS"]), float(seq_len_no_Ns_LS)) * 1000000
+
+        contig_length_dict["1_core"].extend([seq_len_core, seq_len_no_Ns_core, gene_density_core, gene_density_no_Ns_core])
+        contig_length_dict["1_LS"].extend([seq_len_LS, seq_len_no_Ns_LS, gene_density_LS, gene_density_no_Ns_LS])
+    elif seq_id == '2':
+        chr2_core = cur_record.seq[0:FoL_Chr2_LS_start]
+        chr2_LS = cur_record.seq[FoL_Chr2_LS_start:]
+        seq_len_core = len(chr2_core)
+        seq_len_LS = len(chr2_LS)
+        N_count_core = chr2_core.count('N')
+        N_count_LS = chr2_LS.count('N')
+        seq_len_no_Ns_core = int(seq_len_core) - int(N_count_core)
+        seq_len_no_Ns_LS = int(seq_len_LS) - int(N_count_LS)
+        # contig_length_dict["2_core"].extend([seq_len_core, seq_len_no_Ns_core])
+        # contig_length_dict["2_LS"].extend([seq_len_LS, seq_len_no_Ns_LS])
+        gene_density_core = numpy.divide(float(gene_count_dict["2_core"]), float(seq_len_core)) * 1000000
+        gene_density_LS = numpy.divide(float(gene_count_dict["2_LS"]), float(seq_len_LS)) * 1000000
+        gene_density_no_Ns_core = numpy.divide(float(gene_count_dict["2_core"]), float(seq_len_no_Ns_core)) * 1000000
+        gene_density_no_Ns_LS = numpy.divide(float(gene_count_dict["2_LS"]), float(seq_len_no_Ns_LS)) * 1000000
+
+        contig_length_dict["2_core"].extend([seq_len_core, seq_len_no_Ns_core, gene_density_core, gene_density_no_Ns_core])
+        contig_length_dict["2_LS"].extend([seq_len_LS, seq_len_no_Ns_LS, gene_density_LS, gene_density_no_Ns_LS])
+    else:
+        seq_len = len(cur_record.seq)
+        N_count = cur_record.seq.count('N')
+        seq_len_no_Ns = int(seq_len) - int(N_count)
+        # contig_length_dict[seq_id].extend([seq_len, seq_len_no_Ns])
+        # print (str(gene_count_dict[seq_id]) + " - " + str(seq_len))
+        gene_density = numpy.divide(float(gene_count_dict[seq_id]), float(seq_len)) * 1000000
+        # print (gene_density)
+        gene_density_no_Ns = numpy.divide(float(gene_count_dict[seq_id]), float(seq_len_no_Ns)) * 1000000
+        # print (gene_density_no_Ns)
+        # print(seq_id)
+        contig_length_dict[seq_id].extend([seq_len, seq_len_no_Ns, gene_density, gene_density_no_Ns])
+
+
+
+
+
 #-----------------------------------------------------
 # Step 3
 # Print genes containing NLR domains
@@ -176,20 +250,25 @@ print( "\n\nthe total number of NLR genes was:\t" + str(i) )
 # Print genes by domain and by chromosome
 #-----------------------------------------------------
 
-
-
 print("\n\nthe following genes were found in FoL regions:")
 
+# for each chromsome in FoL
 for FoL_contig in sorted(contig_dict):
+    # Get stats on the number of genes containing NLR domains
+    genes_in_contig = contig_dict[FoL_contig]
+    total_genes = gene_count_dict[FoL_contig]
+    # print(FoL_contig)
+    # print(contig_length_dict[FoL_contig])
+    gene_density = int(contig_length_dict[FoL_contig][2])
+    gene_density_adj = int(contig_length_dict[FoL_contig][3])
+    print("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes; with a density of " + str(gene_density) + " genes.Mb-1 or " + str(gene_density_adj) + " adjusted)")
+    file_N_term_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes; with a density of " + str(gene_density) + " genes.Mb-1 or " + str(gene_density_adj) + " adjusted)\n")
+    file_NBD_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes; with a density of " + str(gene_density) + " genes.Mb-1 or " + str(gene_density_adj) + " adjusted)\n")
+    file_C_term_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes; with a density of " + str(gene_density) + " genes.Mb-1 or " + str(gene_density_adj) + " adjusted)\n")
+    # For each chromosome build a dictionary for all the N-terminal, NBD and C-terminal domains
     N_terminal_dict = defaultdict(list)
     NBD_dict = defaultdict(list)
     C_terminal_dict = defaultdict(list)
-    genes_in_contig = contig_dict[FoL_contig]
-    total_genes = gene_count_dict[FoL_contig]
-    print("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes)")
-    file_N_term_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes)" + "\n")
-    file_NBD_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes)" + "\n")
-    file_C_term_out.write("\n\nFoL contig " + str(FoL_contig) + " (" + str(len(genes_in_contig)) + "/ " + str(total_genes) +" genes)" + "\n")
     for line in genes_in_contig:
         split_line = line.split("\t")
         combination = split_line[0].split("---")
@@ -202,27 +281,24 @@ for FoL_contig in sorted(contig_dict):
             NBD_dict[NBD].append(line)
         if C_terminal != 'NA':
             C_terminal_dict[C_terminal].append(line)
-    print("N-terminal")
-    # for keys in sorted(N_terminal_dict, key=lambda keys: len(N_terminal_dict[keys]), reverse=True):
+    # extracting N-terminal domains
     for keys in ["PKinase", "HET", "Peptidase S8", "PND UDP", "Helo-like", "Helo", "C2", "Patatin", "Goodbye", "PFD", "RelA Spot", 'SesA', 'sesB-like']:
         num_genes = len(N_terminal_dict[keys])
         frac_genes = numpy.divide(float(num_genes), float(total_genes)) * 100
-        # print ("\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes))
-        file_N_term_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\n")
-    print("NBD")
-    # for keys in sorted(NBD_dict, key=lambda keys: len(NBD_dict[keys]), reverse=True):
+        frac_density_adj = numpy.divide(float(num_genes), float(gene_density_adj))
+        file_N_term_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\t" + str(frac_density_adj) + "\n")
+    # extracting NBD domains
     for keys in ['P-loop NTPase', 'NACHT', 'NB-ARC', 'AAA']:
         num_genes = len(NBD_dict[keys])
         frac_genes = numpy.divide(float(num_genes), float(total_genes)) * 100
-        # print ("\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes))
-        file_NBD_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\n")
-    print("C-terminal")
-    # for keys in sorted(C_terminal_dict, key=lambda keys: len(C_terminal_dict[keys]), reverse=True):
+        frac_density_adj = numpy.divide(float(num_genes), float(gene_density_adj))
+        file_NBD_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\t" + str(frac_density_adj) + "\n")
+    # extracting C-terminal domains
     for keys in ['Ankyrin', 'TPR', 'WD40', 'WD40-like', 'half TPR']:
         num_genes = len(C_terminal_dict[keys])
         frac_genes = numpy.divide(float(num_genes), float(total_genes)) * 100
-        # print ("\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes))
-        file_C_term_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\n")
+        frac_density_adj = numpy.divide(float(num_genes), float(gene_density_adj))
+        file_C_term_out.write("\t" + FoL_contig + "\t" + str(keys) + "\t" + str(num_genes) + "\t" + str(frac_genes) + "\t" + str(frac_density_adj) + "\n")
 
     del N_terminal_dict
     del NBD

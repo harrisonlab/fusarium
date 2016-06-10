d9794401753fe70c711d0b7f6aabb24f47add105
adarmitage <Andrew.Armitage@EMR.ac.uk>
2016-06-10 19:06:16 +0100

    Not option to give value of plotted point

--- pathogen/identify_LS_chromosomes/circos/gff2circos_scatterplot.py
+++ pathogen/identify_LS_chromosomes/circos/gff2circos_scatterplot.py
@@ -15,9 +15,12 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument('--gff',required=True,type=str,help='A gff file of annotations')
 ap.add_argument('--feature',required=True,type=str,help='The feature you want to extract / plot e.g. gene')
+ap.add_argument('--value',required=False, default=1, type=float,help='The value to be associated with the extracted features')
+
 
 conf = ap.parse_args()
 feat = conf.feature
+value = conf.value
 
 with open(conf.gff) as f:
     gff_lines = f.readlines()
@@ -38,6 +41,6 @@ for line in gff_lines:
         contig = str(split_line[0])
         start = str(split_line[3])
         stop = str(split_line[4])
-        value = str("1")
+        value = str(value)
         feature = "\t".join([contig, start, stop, value])
         print(feature)

06b5eec1f521a3768fd6c8f56489d68f63efb055
adarmitage <Andrew.Armitage@EMR.ac.uk>
2016-09-15 11:48:54 +0100

    allow extraction of multiple features if required - useful for CAZY

--- pathogen/identify_LS_chromosomes/circos/gff2circos_scatterplot.py
+++ pathogen/identify_LS_chromosomes/circos/gff2circos_scatterplot.py
@@ -11,15 +11,16 @@ This program is used to convert gff files into scatterplot data for circos
 
 import sys
 import argparse
+from sets import Set
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--gff',required=True,type=str,help='A gff file of annotations')
-ap.add_argument('--feature',required=True,type=str,help='The feature you want to extract / plot e.g. gene')
+ap.add_argument('--feature',required=True,type=str,nargs='+', help='The feature you want to extract / plot e.g. gene')
 ap.add_argument('--value',required=False, default=1, type=float,help='The value to be associated with the extracted features')
 
 
 conf = ap.parse_args()
-feat = conf.feature
+feat_set = Set(conf.feature)
 value = conf.value
 
 with open(conf.gff) as f:
@@ -37,7 +38,7 @@ for line in gff_lines:
         continue
     line = line.rstrip()
     split_line = line.split("\t")
-    if str(feat) == split_line[2]:
+    if split_line[2] in feat_set:
         contig = str(split_line[0])
         start = str(split_line[3])
         stop = str(split_line[4])

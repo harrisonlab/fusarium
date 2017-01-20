4799e21cee28806e2ad0a5396a3cddf95cb1932f
adarmitage <Andrew.Armitage@EMR.ac.uk>
2017-01-20 19:19:55 +0000

    working script

--- analysis/gene_enrichment/IPR_prep_tables.py
+++ analysis/gene_enrichment/IPR_prep_tables.py
@@ -17,7 +17,7 @@ import argparse
 import re
 import numpy
 from sets import Set
-from Bio import SeqIO
+# from Bio import SeqIO
 from collections import defaultdict
 from operator import itemgetter
 
@@ -53,20 +53,44 @@ print set2_contigs
 # Store gene locations for organism 1 in a dictionary
 #-----------------------------------------------------
 
+set1_count = 0
+set2_count = 0
+set1_dict = defaultdict(int)
+set2_dict = defaultdict(int)
+seen_IPR_set = set()
+
 for line in interpro_lines:
     line = line.rstrip()
     split_line = line.split("\t")
     gene_id = split_line[0]
-    m = re.findall("IPR:......", line)
+    m = re.findall("IPR......", line)
+    # print line
+    # print m
     IPR_set = set(m)
+    # print IPR_set
     if split_line[1] in set1_contigs:
-        for IPR in IPR_set
-            set1_dict[IPR] = +1
-            set1_count = +1
+        for IPR in IPR_set:
+            set1_dict[IPR] += 1
+        set1_count += 1
     elif split_line[1] in set2_contigs:
-        for IPR in IPR_set
-            set2_dict[IPR] = +1
-            set2_count = +1
+        for IPR in IPR_set:
+            set2_dict[IPR] += 1
+        set2_count += 1
+    [seen_IPR_set.add(x) for x in IPR_set]
 
 print set1_count
 print set2_count
+
+for IPR in seen_IPR_set:
+    set1_IPR = set1_dict[IPR]
+    set2_IPR = set2_dict[IPR]
+    set1_others = set1_count - set1_IPR
+    set2_others = set2_count - set2_IPR
+    outline1 = "\t".join([str(IPR), str(set1_IPR), str(set2_IPR)]) + "\n"
+    outline2 = "\t".join(["Other genes", str(set1_others), str(set2_others)]) + "\n"
+
+    outfile = "".join([conf.outdir, "/", IPR, "_fischertable.txt"])
+    # print outfile
+    o = open(outfile, 'w')
+    o.write("".join([outline1, outline2]))
+    o.close()
